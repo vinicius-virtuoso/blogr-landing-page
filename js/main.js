f592421ee0f892becfa00b35dedf92ae9f1ee371
Vinicius Eduardo Virtuoso <viniciusvirtuoso1902@gmail.com>
2021-12-11 16:56:31 -0300

    desafio finalizado

--- js/main.js
+++ js/main.js
@@ -8,8 +8,13 @@ $btnMenu.addEventListener('click', () => {
   $btnMenu.classList.toggle('open__btn');
   $navMenu.classList.toggle('menu__open');
 })
-$navItems.forEach((navLinks, index) => {
-  navLinks.addEventListener('click', ({ target }) => {
-    target.classList.toggle('sub__menu__show');
+$navItems.forEach(($navLinks) => {
+  $navLinks.addEventListener('click', () => {
+    if (!$navLinks.classList.contains('sub__menu__show')) {
+      $navItems.forEach($link => $link.classList.remove('sub__menu__show'))
+      $navLinks.classList.add('sub__menu__show');
+    } else {
+      $navLinks.classList.remove('sub__menu__show');
+    }
   })
 })
